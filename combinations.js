9b346331053fb3189d587bbd86b36eb5da4b9df7
harishnanthan <h.chetta17@gmail.com>
2023-01-29 15:44:14 +0530

    added combinations

---
+++ combinations.js
@@ -0,0 +1,32 @@
+/**
+ * combination:
+ * a collection of things where the order does not matter.
+ * given the set of n things, there are 2n possible combinations
+ *
+ * @param {number[]} elements 
+ * @returns {number[ [] ]}
+ */
+const combinations = (elements) => {
+  if (!elements.length) return [[]];
+
+  let firstElement = elements[0];
+  let restElements = elements.splice(1);
+
+  const combsWithoutFirstElement = combinations(restElements);
+  const combsWithFirstElement = [];
+
+  combsWithoutFirstElement.forEach(comb => {
+    combsWithFirstElement.push([...comb, firstElement])
+  })
+
+  return [...combsWithoutFirstElement, ...combsWithFirstElement];
+
+};
+
+console.log(combinations([1, 2, 3]));
+// [
+//   [],       [ 3 ],
+//   [ 2 ],    [ 3, 2 ],
+//   [ 1 ],    [ 3, 1 ],
+//   [ 2, 1 ], [ 3, 2, 1 ]
+// ]
